1cba71d6e60aa5106d2208a95da553619eb672ee
Nzambaza Rosine <gajurosine1@gmail.com>
2022-01-15 19:24:04 +0200

    Initial Commit

---
+++ aga.js
@@ -0,0 +1,36 @@
+let total =1; count=0;
+while(count <= 10){
+    total +=count;
+    count+=1;
+}
+console.log(total);
+var myname="Rosine";
+console.log(myname);
+var mychar="f";
+console.log(mychar);
+let x=4;
+let y=7;
+console.log(x+y);
+console.log(x=y);
+//object
+var person={firstName:"john",lastName:"Deo",age:30}
+delete person.age;
+console.log(person);
+
+<a href ="javascript:void(
+    Useless link
+    </a>
+   <a href ="javascript:void(document.body.style.backgroundColor='red')">;
+   
+    Click me to change the background color of body to red
+    </a>
+
+
+
+
+
+
+
+
+
+
