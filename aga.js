5bb6f61c69aa164e3521f84c4d3d4aedb5bd0718
Nzambaza Rosine <gajurosine1@gmail.com>
2022-01-19 06:47:55 +0200

    webpage.html

--- aga.js
+++ aga.js
@@ -1,29 +1,145 @@
-let total =1; count=0;
+let total =0; count=1;
 while(count <= 10){
     total +=count;
     count+=1;
 }
-console.log(total);
+
+// console is used to write result
+
+// variable decla give names and types of variable
+// string types
 var myname="Rosine";
 console.log(myname);
-var mychar="f";
-console.log(mychar);
+
+// boolens
+var myday=false;
+console.log(myday);
+
+// numeric types
 let x=4;
 let y=7;
 console.log(x+y);
 console.log(x=y);
-//object
-var person={firstName:"john",lastName:"Deo",age:30}
-delete person.age;
-console.log(person);
 
-<a href ="javascript:void(
-    Useless link
-    </a>
-   <a href ="javascript:void(document.body.style.backgroundColor='red')">;
-   
-    // Click me to change the background color of body to red
-    </a>
+//delete opertors
+// var person={firstName:"john",lastName:"Deo",age:30};
+// var person="John "+" dee";
+// delete person.age;
+// console.log(person);
+
+// condition operators
+var age=20;
+while(age<18);
+console.log("old enought");
+
+// Array
+var friut=["mangoes","apples","bananaes"];
+console.log(friut);
+
+// javascript function it's executed in curl blsckets
+var mycar = "vavo"
+function myfunction(n){
+     var mycar = "vavo";
+}
+console.log(mycar);
+// ?????????????
+
+// control statement
+var age = 17;
+if( age > 18) {
+    console.log("allowed to drink beer");
+}
+ else {
+    console.log("not allowed to drink beer");
+}
+
+// if..else
+var food="meat";
+if(food=="cassava"){
+    console.log (cassava);
+
+}else if(food=="irish patatoes"){
+    console.log("irish patatoes");
+
+}else if(food=="meat"){
+    console.log("meat");
+
+}else(food=="beans");{
+    console.log("beans");
+}
+
+// switch case is the same as else if
+
+var place="1";
+switch(place){
+    case"1":console.log("good place");
+    break;
+
+    case"2":console.log("bad place");
+    break;
+    case"3":console.log("beatiful place");
+    break;
+    case"4":console.log("wild\bflowers");
+    break;
+    default:console.log("unexpected")
+}
+// nested if
+// if((x===y ||a===g)&& c===d ){
+    // g=h;
+// }else{
+    // e=f;
+// }
+// looping for
+var count=1; total=0;
+for( count= 0 ; count < 10; count++){
+    console.log(count);
+}
+// do looping
+var count=1;
+do {
+    console.log(count);
+    count++;
+}
+while(count<7);
+
+var count =0;
+do{
+    console.log("count");
+    count++;
+}
+while(count>2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// varr do..while
+
+
+
 
 
 
